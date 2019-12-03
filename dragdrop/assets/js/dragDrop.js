e3f61d23dcb3cfeff56a41fcba4644b868a8b2cc
stefanypedrosa <stefany.pedrosa@fatec.sp.gov.br>
2019-12-03 14:44:12 -0300

    dropdrag

--- dragdrop/assets/js/dragDrop.js
+++ dragdrop/assets/js/dragDrop.js
@@ -1,23 +1,19 @@
 function pegaItem(evt){
-    console.log("comecei a arrastar "+evt.target.id);
-    // adicionei o id do item que vou arrastar na estrutura dataTransfer
     evt.dataTransfer.setData("TAREFA",evt.target.id);
 }
 
 function habilitaArrastar(evt){
     evt.preventDefault();
-    //console.log("Estou sobre um determinado ponto que vai receber a tarefa");
 }
 
 function recebeItem(evt){
-    // agora preciso recuperar a TAREFA que estava no dataTransfer e adicioná-la na coluna
     var idTarefa = evt.dataTransfer.getData("TAREFA");
     var tarefa = document.getElementById(idTarefa);
-    console.log("Dropei a tarefa aqui ="+evt.target.id);
-
-
-    // crio um elmento DOM e insiro na coluna correspondente
     evt.target.appendChild(tarefa);
-
-
 } 
+
+function adcTarefa(){
+    var task = document.getElementById("tarefa").value;
+    var num = 1;
+    document.getElementById("pendentes").innerHTML += '<div class="itemTarefa" draggable="true" ondragstart="pegaItem(event);" id="t'+num+'">'+task+'</div>';
+}
